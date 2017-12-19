9cf5e8cb2e1d330eceb01e917403d050d3a42661
Administrator <Administrator@USER-20170227MD>
2017-12-19 10:35:57 +0800

    解决collector类名无法使用requestMapping的问题 （@ComponentScan("cn.zl.control")） 并且可以同时支持thymeleaf跟jsp

--- play/src/main/java/cn/zl/Application.java
+++ play/src/main/java/cn/zl/Application.java
@@ -3,12 +3,14 @@ package cn.zl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+//@ComponentScan("cn.zl.control")
 public class Application {
 	
 	public static void main(String[] args) {
--- play/src/main/java/cn/zl/control/Demo.java
+++ play/src/main/java/cn/zl/control/Demo.java
@@ -1,35 +1,37 @@
 package cn.zl.control;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import cn.zl.entity.Dept;
 import cn.zl.mapper.DeptMapper;
 
-@RestController
+@Controller
+@RequestMapping("/demo")
 public class Demo {
 
 	@Autowired
 	private DeptMapper deptMapper;
 	
-	@RequestMapping(name="/heloHtml", method=RequestMethod.GET)
+	@RequestMapping(value = "/hello", method=RequestMethod.GET)
 	public String hello(HttpServletRequest request, Map<String,Object> map) {
 		
-		/*try {
-			List<Dept> list = deptMapper.getAll();
-			request.setAttribute("list", list);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}*/
-		 map.put("hello","from Demo.helloHtml"); 
-		 return"/helloHtml"; 
+		 map.put("hello","from Demo.helloHtml");
+		 return "/helloHtml"; 
+	}
+	
+	@RequestMapping(value = "/world", method=RequestMethod.GET)
+	public String world(HttpServletRequest request, Map<String,Object> map) {
+		
+		 map.put("hello","from Demo.helloHtml");
+		 return "/helloHtml"; 
 	}
 }
