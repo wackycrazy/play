dc102b6fc791d7c7047b67a33c72b324802d6511
Administrator <Administrator@USER-20170227MD>
2018-06-26 17:46:51 +0800

    添加jwt登录验证

--- play/src/main/java/cn/zl/mapper/BonusMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.zl.mapper;
-
-import cn.zl.entity.Bonus;
-
-public interface BonusMapper {
-    int insert(Bonus record);
-
-    int insertSelective(Bonus record);
-}
--- play/src/main/java/cn/zl/mapper/SalgradeMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.zl.mapper;
-
-import cn.zl.entity.Salgrade;
-
-public interface SalgradeMapper {
-    int insert(Salgrade record);
-
-    int insertSelective(Salgrade record);
-}
